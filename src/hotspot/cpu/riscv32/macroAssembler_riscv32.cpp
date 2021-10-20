9baa42641fa7897aa3451a190df852e70b376986
Ningning Shi <shiningning@iscas.ac.cn>
2021-10-20 11:56:09 +0800

    Clean the sllw/slliw/sraw/sraiw/srlw/srliw
    Use the sll to instead the sllw,
    use the slli to insted the slliw,
    use the sra to insated the sraw,
    use the srai to instead the sraiw,
    use the srl to instead the srlw,
    use the srli to instead the srliw.
    
    After this patch, the RV32 code will be cleaned in
    the sllw/slliw/sraw/sraiw/srlw/srliw.

--- src/hotspot/cpu/riscv32/macroAssembler_riscv32.cpp
+++ src/hotspot/cpu/riscv32/macroAssembler_riscv32.cpp
@@ -3321,10 +3321,10 @@ void MacroAssembler::string_compare(Register str1, Register str2,
   // Bizzarely, the counts are passed in bytes, regardless of whether they
   // are L or U strings, however the result is always in characters.
   if (!str1_isL) {
-    sraiw(cnt1, cnt1, 1);
+    srai(cnt1, cnt1, 1);
   }
   if (!str2_isL) {
-    sraiw(cnt2, cnt2, 1);
+    srai(cnt2, cnt2, 1);
   }
 
   // Compute the minimum of the string lengths and save the difference in result.
