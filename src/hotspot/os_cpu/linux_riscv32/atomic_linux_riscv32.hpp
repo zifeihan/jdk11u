e1e28cb708df51ff89f65a943738bcf4f0004534
zhangxiang-plct <zhangxiang@nj.iscas.ac.cn>
2022-02-21 17:32:22 +0800

    Fix pr170 on PlatformLoad and PlatformStore

--- src/hotspot/os_cpu/linux_riscv32/atomic_linux_riscv32.hpp
+++ src/hotspot/os_cpu/linux_riscv32/atomic_linux_riscv32.hpp
@@ -110,13 +110,21 @@ inline T Atomic::PlatformCmpxchg<4>::operator()(T exchange_value,
   return rv;
 }
 
-/*template<>
+template<>
 template<typename T>
-inline T Atomic::PlatformLoad<8>::operator()(T const volatile* src) const {}
+inline T Atomic::PlatformLoad<8>::operator()(T const volatile* src) const {
+  STATIC_ASSERT(8 == sizeof(T));
+  volatile int64_t dest;
+  os::atomic_copy64(reinterpret_cast<const volatile int64_t*>(src), reinterpret_cast<volatile int64_t*>(&dest));
+  return PrimitiveConversions::cast<T>(dest);
+}
 
 template<>
 template<typename T>
 inline void Atomic::PlatformStore<8>::operator()(T store_value,
-                                                 T volatile* dest) const {}
-*/
+                                                 T volatile* dest) const {
+  STATIC_ASSERT(8 == sizeof(T));
+  os::atomic_copy64(reinterpret_cast<const volatile int64_t*>(&store_value), reinterpret_cast<volatile int64_t*>(dest));
+}
+
 #endif // OS_CPU_LINUX_RISCV32_VM_ATOMIC_LINUX_RISCV32_HPP
